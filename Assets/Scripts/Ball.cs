fba83f1a7fea402341cc7204eb3c5e5976f1df40
ZachNich <raiste.doctow@gmail.com>
2021-07-05 15:24:04 -0500

    add second level

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    Vector2 paddleToBallVector;
+    Rigidbody2D rb;
+    bool hasStarted = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle1.transform.position;    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasStarted)
+        {
+            LockToPaddle();
+            LaunchOnClick();
+        }
+    }
+
+    void LockToPaddle()
+    {
+        var paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.velocity = new Vector2(xPush, yPush);
+        }
     }
 }
