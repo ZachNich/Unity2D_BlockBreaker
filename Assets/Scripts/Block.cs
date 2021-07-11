65e820d3e47804b10485b1587ff1c9b6fc5d7c1d
ZachNich <raiste.doctow@gmail.com>
2021-07-11 11:57:53 -0500

    add multihit blocks and sprites

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,26 +5,45 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject sparklesVFX;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
+
     Level level;
     GameStatus gameStatus;
+    SpriteRenderer spriteRenderer;
+
+    int timesHit;
 
     void Start()
     {
         level = FindObjectOfType<Level>();
         gameStatus = FindObjectOfType<GameStatus>();
-        level.AddBlock();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (tag == "Breakable") level.AddBlock();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        HandleBlockHit();
+        if (tag == "Breakable") {
+            timesHit++;
+            if (timesHit >= maxHits) HandleBlockDestroy();
+            else spriteRenderer.sprite = hitSprites[timesHit - 1];
+        }
     }
 
-    void HandleBlockHit()
+    void HandleBlockDestroy()
     {
         gameStatus.AddToScore();
+        TriggerSparkles();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.RemoveBlock();
+    }
+
+    void TriggerSparkles()
+    {
+        GameObject sparkles = Instantiate(sparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 2f);
     }
 }
